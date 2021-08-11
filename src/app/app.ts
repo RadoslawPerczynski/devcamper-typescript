9d37154fa815c8f60e6ec1453c51e967bed675bf
Radoslaw Perczynski <radoslaw.perczynski@casasoft.com.mt>
2021-08-11 03:10:21 +0200

    setting up the project skeleton with typescript

---
+++ src/app/app.ts
@@ -0,0 +1,56 @@
+import express, { Application, Request, Response, NextFunction } from 'express';
+
+// var createError = require('http-errors');
+// var express = require('express');
+// var path = require('path');
+// var cookieParser = require('cookie-parser');
+// var logger = require('morgan');
+
+var indexRouter = require('./routes/home');
+var usersRouter = require('./routes/users');
+// var usersRouter = require('./routes/users');
+// var indexRouter = require('./routes/home');
+
+// ==================
+// view engine setup
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
+// app.use(logger('dev'));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// catch 404 and forward to error handler
+// app.use(function (req: Request, res: Response, next: NextFunction) {
+//   next(createError(404));
+// });
+
+// error handler
+// app.use(function (err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
+
+export const app: Application = express();
+
+const add = (a: number, b: number): number => {
+  return a + b;
+};
+
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+  console.log(add(4, 10));
+  res.send('Hello WOrld');
+});
+
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
+
+// app.listen(5000, () => console.log('Server running'));
+
+// module.exports = app;
