cd986dc792c91252233814259991cfe9634e7dc0
Radoslaw Perczynski <radoslaw.perczynski@casasoft.com.mt>
2021-10-05 22:22:21 +0200

    changing the controller to be a class

--- src/app/controllers/bootcampsController.controller.ts
+++ src/app/controllers/bootcampsController.controller.ts
@@ -1,55 +1,59 @@
 import { Request, Response, NextFunction } from 'express';
 
-/**
- * @desc Get all bootcamps
- * @route GET api/v1/bootcamps
- * @access Public
- */
-export const getAllBootcamps = (req: Request, res: Response) => {
-  res.status(200).json({ success: true, msg: 'Get all Bootcamps' });
-};
+class BootcampController {
+  /**
+   * @desc Get all bootcamps
+   * @route GET api/v1/bootcamps
+   * @access Public
+   */
+  getAllBootcamps = (req: Request, res: Response) => {
+    res.status(200).json({ success: true, msg: 'Get all Bootcamps' });
+  };
 
-/**
- * @desc Get single bootcamp
- * @route GET api/v1/bootcamp/:id
- * @access Public
- */
-export const getBootcamp = (req: Request, res: Response) => {
-  const bootcampID: string = req.params.id;
-  res
-    .status(200)
-    .json({ success: true, msg: `Get Single Bootcamp by ID: ${bootcampID}` });
-};
+  /**
+   * @desc Get single bootcamp
+   * @route GET api/v1/bootcamp/:id
+   * @access Public
+   */
+  getBootcamp = (req: Request, res: Response) => {
+    const bootcampID: string = req.params.id;
+    res
+      .status(200)
+      .json({ success: true, msg: `Get Single Bootcamp by ID: ${bootcampID}` });
+  };
 
-/**
- * @desc Create a new bootcamp
- * @route POST api/v1/bootcamps
- * @access Private
- */
-export const createBootcamp = (req: Request, res: Response) => {
-  res.status(200).json({ success: true, msg: 'Create a new bootcamp' });
-};
+  /**
+   * @desc Create a new bootcamp
+   * @route POST api/v1/bootcamps
+   * @access Private
+   */
+  createBootcamp = (req: Request, res: Response) => {
+    res.status(200).json({ success: true, msg: 'Create a new bootcamp' });
+  };
 
-/**
- * @desc Update a bootcamp
- * @route PUT api/v1/bootcamp/:id
- * @access Private
- */
-export const updateBootcamp = (req: Request, res: Response) => {
-  const bootcampID: string = req.params.id;
-  res
-    .status(200)
-    .json({ success: true, msg: `Update the bootcamp of ID: ${bootcampID}` });
-};
+  /**
+   * @desc Update a bootcamp
+   * @route PUT api/v1/bootcamp/:id
+   * @access Private
+   */
+  updateBootcamp = (req: Request, res: Response) => {
+    const bootcampID: string = req.params.id;
+    res
+      .status(200)
+      .json({ success: true, msg: `Update the bootcamp of ID: ${bootcampID}` });
+  };
 
-/**
- * @desc Delete bootcamp
- * @route DELETE api/v1/bootcamp/:id
- * @access Private
- */
-export const deleteBootcamp = (req: Request, res: Response) => {
-  const bootcampID: string = req.params.id;
-  res
-    .status(200)
-    .json({ success: true, msg: `Delete the bootcamp of ID: ${bootcampID}` });
-};
+  /**
+   * @desc Delete bootcamp
+   * @route DELETE api/v1/bootcamp/:id
+   * @access Private
+   */
+  deleteBootcamp = (req: Request, res: Response) => {
+    const bootcampID: string = req.params.id;
+    res
+      .status(200)
+      .json({ success: true, msg: `Delete the bootcamp of ID: ${bootcampID}` });
+  };
+}
+
+export const bootcampController = new BootcampController();
--- src/app/routes/bootcamps.routing.ts
+++ src/app/routes/bootcamps.routing.ts
@@ -1,19 +1,17 @@
 import express, { Request, Response, NextFunction } from 'express';
-import {
-  createBootcamp,
-  deleteBootcamp,
-  getAllBootcamps,
-  getBootcamp,
-  updateBootcamp,
-} from '../controllers/bootcampsController.controller';
+import { bootcampController } from '../controllers/bootcampsController.controller';
 const router = express.Router();
 
-router.route('/').get(getAllBootcamps).post(createBootcamp);
+// router.route('/').get(getAllBootcamps).post(createBootcamp);
+router
+  .route('/')
+  .get(bootcampController.getAllBootcamps)
+  .post(bootcampController.createBootcamp);
 
 router
   .route('/:id')
-  .get(getBootcamp)
-  .put(updateBootcamp)
-  .delete(deleteBootcamp);
+  .get(bootcampController.getBootcamp)
+  .put(bootcampController.updateBootcamp)
+  .delete(bootcampController.deleteBootcamp);
 
 export { router as bootcampsRouting };
