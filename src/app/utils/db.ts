73c6b0f12e02ecd078d3caab5a22ecd92755a194
Radoslaw Perczynski <radoslaw.perczynski@casasoft.com.mt>
2021-10-07 22:10:13 +0200

    removing mongoose types - realised they are not necessary

--- src/app/utils/db.ts
+++ src/app/utils/db.ts
@@ -5,9 +5,14 @@ import colors from 'colors';
 export const connectDB = async () => {
   try {
     const connection = await mongoose.connect(envVariables.MONGO_URI as string);
+
     console.log(
       `MongoDB connected: ${connection.connection.host}`.cyan.underline.bold
     );
+
+    mongoose.connection.on('error', (err) => {
+      console.log(colors.red.underline(`${err}`));
+    });
   } catch (error) {
     console.log(colors.red.underline(`${error}`));
   }
