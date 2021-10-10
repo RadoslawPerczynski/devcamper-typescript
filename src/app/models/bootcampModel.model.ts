7e6b4669570e36bec30335a1b3fa429fcab47458
Radoslaw Perczynski <radoslaw.perczynski@casasoft.com.mt>
2021-10-10 11:58:06 +0200

    updated the model - more properties added

--- src/app/models/bootcampModel.model.ts
+++ src/app/models/bootcampModel.model.ts
@@ -1,14 +1,39 @@
-import { Schema, model, connect } from 'mongoose';
+import { Schema, model } from 'mongoose';
 
-// 1. Create an interface representing a document in MongoDB.
+const pointSchema = new Schema({
+  type: {
+    type: String,
+    enum: ['Point'],
+    required: true,
+  },
+  coordinates: {
+    type: [Number],
+    required: true,
+  },
+  formattedAddress: String,
+  street: String,
+  city: String,
+  state: String,
+  zipCode: String,
+  country: String,
+});
+
 interface Bootcamp {
   name: string;
   description: string;
+  website: string;
+  email: string;
   slug: string;
+  address: string;
+  location: object; // GeoJson point
+  careers: string[];
+  averageRating: number;
+  averageCost: number;
+  photo: string;
+  housing: boolean;
 }
 
-// 2. Create a Schema corresponding to the document interface.
-const bootcampSchema = new Schema<Bootcamp>(
+const BootcampSchema = new Schema<Bootcamp>(
   {
     name: {
       type: String,
@@ -23,6 +48,48 @@ const bootcampSchema = new Schema<Bootcamp>(
       required: true,
       maxlength: [500, 'Description cannot be more than 50 characters'],
     },
+    slug: String,
+    website: {
+      type: String,
+      required: true,
+      match: [
+        /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/,
+        'Please input a valid website address',
+      ],
+    },
+    email: {
+      type: String,
+      required: true,
+      match: [/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/, 'Please add a valid email'],
+    },
+    address: {
+      type: String,
+      required: [true, 'Please add an address'],
+    },
+    location: {
+      type: pointSchema,
+      // required: true,
+      index: '2dsphere',
+    },
+    careers: {
+      type: [String],
+      required: true,
+      enum: ['Web Development', 'Mobile Development', 'UI/UX'],
+    },
+    averageRating: {
+      type: Number,
+      min: [1, 'Rating must be at least 1'],
+      max: [1, 'Rating must not be more than 10'],
+    },
+    averageCost: Number,
+    photo: {
+      type: String,
+      default: 'no-photo.jpg',
+    },
+    housing: {
+      type: Boolean,
+      default: false,
+    },
   },
   {
     timestamps: true, // adds created_at & updated_at
@@ -30,4 +97,4 @@ const bootcampSchema = new Schema<Bootcamp>(
 );
 
 // 3. Create a Model.
-export const BootcampModel = model<Bootcamp>('Bootcamp', bootcampSchema);
+export const BootcampModel = model<Bootcamp>('Bootcamp', BootcampSchema);
